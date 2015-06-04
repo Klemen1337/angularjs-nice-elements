6fd261c30b29dcd5c1ae51833e2d8694f5615c98
Klemen Kastelic <klemen.kastelic@xlab.si>
2015-06-04 10:35:19 +0200

    Loader updated

--- dist/nice.js
+++ dist/nice.js
@@ -1758,11 +1758,26 @@ angular.module('niceElements').run(['$templateCache', function($templateCache) {
 
 
   $templateCache.put('views/nice-loader.html',
+    "<!--<div class=\"nice-loader\" ng-if=\"!visibleWhen\" ng-class=\"addClass\">-->\n" +
+    "    <!--<div class=\"spinner\">-->\n" +
+    "      <!--<div class=\"double-bounce1\"></div>-->\n" +
+    "      <!--<div class=\"double-bounce2\"></div>-->\n" +
+    "    <!--</div>-->\n" +
+    "<!--</div>-->\n" +
+    "\n" +
     "<div class=\"nice-loader\" ng-if=\"!visibleWhen\" ng-class=\"addClass\">\n" +
-    "    <div class=\"spinner\">\n" +
-    "      <div class=\"double-bounce1\"></div>\n" +
-    "      <div class=\"double-bounce2\"></div>\n" +
-    "    </div>\n" +
+    "  <svg version=\"1.1\" id=\"loader-1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n" +
+    "     width=\"40px\" height=\"40px\" viewBox=\"0 0 50 50\" style=\"enable-background:new 0 0 50 50;\" xml:space=\"preserve\">\n" +
+    "  <path fill=\"#000\" d=\"M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z\">\n" +
+    "    <animateTransform attributeType=\"xml\"\n" +
+    "      attributeName=\"transform\"\n" +
+    "      type=\"rotate\"\n" +
+    "      from=\"0 25 25\"\n" +
+    "      to=\"360 25 25\"\n" +
+    "      dur=\"0.6s\"\n" +
+    "      repeatCount=\"indefinite\"/>\n" +
+    "    </path>\n" +
+    "  </svg>\n" +
     "</div>"
   );
 
