a56f6fbb2449661a172cebb6418a103b26fa5bb5
Klemen Kastelic <klemen.kastelic@xlab.si>
2015-08-13 15:24:30 +0200

    Added NiceNotification factory

--- demo/home/HomeCtrl.js
+++ demo/home/HomeCtrl.js
@@ -1,12 +1,51 @@
 'use strict';
 
-angular.module('myApp').controller('HomeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q) {
+angular.module('myApp').controller('HomeCtrl', function($scope, $http, $q, NiceNotification) {
     $scope.listObjs = [{id:1, value:"Apple"}, {id:2, value:"Banana"}, {id:3, value:"Orange"}];
     $scope.listStrings = ["String 1", "String 2", "String 3"];
 
     $scope.percentage = 0.22000000;
 
+    // Notification tests
+    $scope.primary = function() {
+        NiceNotification('Primary notification');
+    };
 
+    $scope.error = function() {
+        NiceNotification.error({message:'Error notification', delay: 20000});
+    };
+
+    $scope.success = function() {
+        NiceNotification.success('Success notification');
+    };
+
+    $scope.info = function() {
+        NiceNotification.info('Information notification');
+    };
+
+    $scope.warning = function() {
+        NiceNotification.warning('Warning notification');
+    };
+
+    $scope.primaryTitle = function() {
+        NiceNotification({message: 'Primary notification', title: 'Primary notification'});
+    };
+
+    $scope.errorTime = function() {
+        NiceNotification.error({message: 'Error notification 1s', delay: 1000});
+    };
+
+    $scope.successTime = function() {
+        NiceNotification.success({message: 'Success notification 20s', delay: 20000});
+    };
+
+    $scope.errorHtml = function() {
+        NiceNotification.error({message: '<b>Error</b> <s>notification</s>', title: 'Html', delay: 20000});
+    };
+
+    $scope.successHtml = function() {
+        NiceNotification.success({message: 'Success notification<br>Some other <b>content</b><br><a href="https://github.com/alexcrack/angular-ui-notification">This is a link</a><br><img src="https://angularjs.org/img/AngularJS-small.png">', title: 'Html content'});
+    };
 
 
     $scope.getAddresses = function(address) {
@@ -33,4 +72,4 @@ angular.module('myApp').controller('HomeCtrl', ['$scope', '$http', '$q', functio
             scrollTop: $("#"+destination).offset().top - 50
         }, 300);
     };
-}]);
+});
