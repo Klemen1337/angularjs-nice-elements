21c7ef291881cb52169fe0abd9c0eed65fe393e4
Klemen Kastelic <klemen@olaii.com>
2022-11-16 10:18:16 +0100

    - added assets to dev server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 var port = 3031;
-var host ='localhost';
+var host = 'localhost';
 
 app.use('/demo', express.static('demo'));
 app.use('/node_modules', express.static('node_modules'));
@@ -12,9 +12,10 @@ app.use('/dist', express.static('dist'));
 app.use('/src', express.static('src'));
 app.use('/views', express.static('src/views'));
 app.use('/filters', express.static('src/filters'));
+app.use('/assets', express.static('demo/assets'));
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/demo/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/demo/index.html');
 });
 
 app.listen(port);
