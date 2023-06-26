29a8908c57abe62c7091c71e1783239887e44883
Klemen Kastelic <klemen@olaii.com>
2023-06-26 11:18:44 +0200

    Version 1.8.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v1.8.7 (26.6.2023)
+
+- changed trash can with x - OLA-639
+
 ## v1.8.6 (8.6.2023)
 
 - worked on nice-dropdown filter function,
--- src/services/nice-service.js
+++ src/services/nice-service.js
@@ -10,7 +10,7 @@ angular.module('niceElements')
   .service('NiceService', function () {
     var service = {
       name: "Nice elements",
-      version: "1.8.6",
+      version: "1.8.7",
       getHeader: function () {
         return {};
       }
