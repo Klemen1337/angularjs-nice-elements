5220b787ed075e0f06d326311d8c6332dc174ca6
Klemen Kastelic <klemen@olaii.com>
2023-12-05 12:26:39 +0100

    - worked on demo page

--- demo/components/navigation/navigation.js
+++ demo/components/navigation/navigation.js
@@ -10,11 +10,19 @@ angular.module('niceElementsDemo')
   .directive('navigation', function () {
     return {
       restrict: 'E',
-      replace: true,
       templateUrl: 'demo/components/navigation/navigation.html',
       controller: function ($scope, $route, $location, NiceService) {
         $scope.niceService = NiceService;
         $scope.routes = $route.routes;
+        $scope.isOpen = true;
+
+        $scope.close = function () {
+          $scope.isOpen = false;
+        }
+
+        $scope.toggle = function () {
+          $scope.isOpen = !$scope.isOpen;
+        }
 
         $scope.location = $location.path();
         $scope.$on('$routeChangeStart', function (evt, toState, toParams, fromState, fromParams) {
